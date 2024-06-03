92c44e6d436884f4a9111922955e7dbb2161f0e5
hoangdh9104 <hoangkind9104@gmail.com>
2024-06-03 16:46:19 +0700

    using bramus/router

--- routes/client.php
+++ routes/client.php
@@ -1,10 +1,23 @@
 <?php
 
-// Define routes
-$router->get('/', function () {
-    echo "Home";
-});
+// Website có các trang: Trang chủ, Giới Thiệu, Sản phẩm, Chi tiết sản phẩm, Liên hệ
 
-$router->get('/about', function () {
-    echo "Giới thiệu";
-});
+// Để định nghĩa được, điều đầu tiên là phải tạo Controller trước
+// Tiếp theo, khai báo function tương ứng để xử lý
+// Bước cuối, định nghĩa đường dẫn
+
+// HTTP Method: get(lấy dữ liệu), post(lưu, tạo mới một tài nguyên), push/path (update-đa số dùng push), delete(xóa), option, head (bỏ qua- khi làm việc font-end framework)
+
+use Hoang9104\Setup\Controllers\Client\AboutController;
+use Hoang9104\Setup\Controllers\Client\ContactController;
+use Hoang9104\Setup\Controllers\Client\HomeController;
+use Hoang9104\Setup\Controllers\Client\ProductController;
+
+$router->get('/',                       HomeController::class               .'@index');
+$router->get('/about',                  AboutController::class              .'@index');
+
+$router->get('/contact',                ContactController::class            .'@index');
+$router->post('/contact/store',         ContactController::class            .'@store');
+
+$router->get('/products',               ProductController::class            .'@index');
+$router->get('/products/{id}',          ProductController::class            .'@detail');
---
+++ src/Controllers/Client/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hoang9104\Setup\Controllers\Client;
+
+use Hoang9104\Setup\Commons\Controller;
+
+class HomeController extends Controller{
+    public function index(){
+        echo __CLASS__ . '@' . __FUNCTION__;
+
+    }
+}
