c40c62fafa018833e1413ec6fea60f226958b95e
hoangdh9104 <hoangkind9104@gmail.com>
2024-06-08 10:51:59 +0700

    update

--- src/Controllers/Client/HomeController.php
+++ src/Controllers/Client/HomeController.php
@@ -5,10 +5,12 @@ namespace Hoang9104\Setup\Controllers\Client;
 
 use Hoang9104\Setup\Commons\Controller;
 
+
 class HomeController extends Controller
 {
     public function index()
     {
+
         $name = 'Hoangdh9104';
         $this->renderViewClient('home', [
             'name' => $name,
---
+++ src/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hoang9104\Setup\Models;
+
+use Hoang9104\Setup\Commons\Model;
+
+class User extends Model
+{
+    protected string $tableName = 'users';
+    
+}
